0b950bff2263af59be9b5541d6c92ccea91ca0cb
Juan Guillermo Florez <juangflorezm@gmail.com>
2019-03-06 20:09:07 -0500

    Rutas agregadas

---
+++ app/Http/Controllers/SpotifyController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SpotifyController extends Controller
+{
+    function lanzamientos()
+    {
+
+    }
+
+    function artistas($id)
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Lanzamientos', 'SpotifyController@lanzamientos');
+
+Route::get('/artistas/​{id}', 'SpotifyController@artistas');
