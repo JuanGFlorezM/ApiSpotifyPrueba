ba5788dd4fce2eb50f153d6eac72db41f4e397a4
Juan Guillermo Florez <juangflorezm@gmail.com>
2019-03-07 18:57:01 -0500

    Cambio de página de inicio

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*Route::get('/', function () {
+    return view('lanzamientos');
+});*/
+
+Route::get('/', 'SpotifyController@lanzamientos');
 
 Route::get('lanzamientos', 'SpotifyController@lanzamientos');
 
