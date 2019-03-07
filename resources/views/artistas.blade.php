d82cf63ed96c2b31ff4662da10763bbaf975fad3
Juan Guillermo Florez <juangflorezm@gmail.com>
2019-03-07 12:36:28 -0500

    Modificación de estilo
    títulos, textos y tamaño de imagenes

--- resources/views/artistas.blade.php
+++ resources/views/artistas.blade.php
@@ -1,29 +1,55 @@
 <!DOCTYPE html>
 <html>  
     <head>
+            <title> {{$artistas[0]->name}}</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	</head>
 	<body>
-            <div class = "row"> 
-                    <div class="card col-3" style="width: 1rem;">
-                    <img class="card-img-top" src="{{$artistas[0]->images[0]->url}}" alt="Card image cap">
-                    <div class="card-body">
-                      {{$artistas[0]->name}}
-                       
-                    </div>
-                    </div>
-                        </div>
+        <div style="background-color: #eceff1;">
+            <div class="container text-center">
+                    <h2> {{$artistas[0]->name}}</h2>
+        </div>
+        </div>
+        <div class = "row"> 
+            <div class="card col-2" style="width: 1rem;">
+                <img class="card-img-top" src="{{$artistas[0]->images[0]->url}}" alt="Card image cap">
+                <div class="card-body text-center">
+                    {{$artistas[0]->name}}                       
+                </div>
+            </div>
+        </div>
+
         <div class = "row">
         @foreach ($albumes as $item)
-        <div class="card col-3" style="width: 1rem;">
+            <div class="card col-2" style="width: 1rem;">
                 <img class="card-img-top" src="{{$item->images[0]->url}}" alt="Card image cap">
-                <div class="card-body">
-                        {{$item->name}}
-                   <a href= "/artistas/{{$item->artists[0]->id}}">{{$item->artists[0]->name}}</a>
+                <div class="card-title text-center">
+                    <strong>{{$item->name}}</strong>                    
                 </div>
+                <div class="card-text text-center">                        
+                    <a href= "/artistas/{{$item->artists[0]->id}}">{{$item->artists[0]->name}}</a>
                 </div>
+            </div>
         @endforeach
     </div>
-
+        <!-- Footer -->
+        <footer class="page-footer font-small blue pt-4">
+            <div style="background-color: #eceff1;">
+            <!-- Footer Links -->
+            <div class="container-fluid text-center text-md-left">
+        
+            <!-- Grid row -->
+            <div class="row">        
+                <!-- Grid column -->
+                <div class="col-md-6 mt-md-0 mt-3">        
+                <!-- Content -->
+                <h5 class="text-uppercase"><strong>PHP - CDI-Interactive</strong></h5>
+                <p>Prueba realizada por Juan Guillermo Flórez</p>        
+                </div>                
+            <!-- Grid row -->        
+            </div>  
+        </div>          
+        </footer>
+      <!-- Footer -->
     </body>
 </html>
--- resources/views/lanzamientos.blade.php
+++ resources/views/lanzamientos.blade.php
@@ -1,20 +1,47 @@
 <!DOCTYPE html>
 <html>  
     <head>
+        <title> Nuevos Lanzamientos</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	</head>
+    </head>
 	<body>
+        <div style="background-color: #eceff1;">
+            <div class="container text-center">
+                <h2> NUEVOS LANZAMIENTOS</h2>
+            </div>
+        </div>
         <div class = "row">
-        @foreach ($lanzamientos as $item)
-        <div class="card col-3" style="width: 1rem;">
-                <img class="card-img-top" src="{{$item->images[0]->url}}" alt="Card image cap">
-                <div class="card-body">
-                        {{$item->name}}
-                   <a href= "/artistas/{{$item->artists[0]->id}}">{{$item->artists[0]->name}}</a>
+            @foreach ($lanzamientos as $item)
+                <div class="card col-2" style="width: 1rem;">
+                    <img class="card-img-top" src="{{$item->images[0]->url}}" alt="Card image cap">
+                        <div class="card-title text-center">
+                            <strong>{{$item->name}}</strong>                          
+                        </div>
+                        <div class="card-text text-center">                                
+                            <a href= "/artistas/{{$item->artists[0]->id}}">{{$item->artists[0]->name}}</a>
+                        </div>
                 </div>
-                </div>
-        @endforeach
-    </div>
-
+            @endforeach
+        </div>
+            
+        <!-- Footer -->
+        <footer class="page-footer font-small blue pt-4">
+                <div style="background-color: #eceff1;">
+            <!-- Footer Links -->
+            <div class="container-fluid text-center text-md-left">
+        
+            <!-- Grid row -->
+            <div class="row">        
+                <!-- Grid column -->
+                <div class="col-md-6 mt-md-0 mt-3">        
+                <!-- Content -->
+                <h5 class="text-uppercase"><strong>PHP - CDI-Interactive</strong></h5>
+                <p>Prueba realizada por Juan Guillermo Flórez</p>        
+                </div>                
+            <!-- Grid row -->        
+            </div>  
+        </div>          
+        </footer>
+      <!-- Footer -->
     </body>
 </html>
